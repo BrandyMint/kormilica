52a023e8249b19dbb57ead4517ddfe940ecf30d0
Paramount2009 <Paramount2009@ya.ru>
2014-04-08 11:29:56 +0300

    modified catalog

--- lp1/config.rb
+++ lp1/config.rb
@@ -7,7 +7,7 @@ sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
-add_import_path "C:/project/kormilica/components"
+add_import_path "C:/project/kormilica/shared/components"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
